2975c7eea043a78bfc5f14ce391478675b77d50a
steam0777 <steamluk07@gmail.com>
2020-06-25 19:43:16 -0400

    docs agregados

---
+++ docs/precache-manifest.8c29a6dc416b07fc4ec74d48d477ff9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03bb3cd9d4c16a9b413ab610739e9dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38ed01bef67074121f44",
+    "url": "/static/css/main.c8cea5e4.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ed01bef67074121f44",
+    "url": "/static/js/main.576e1736.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
